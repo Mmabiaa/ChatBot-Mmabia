bac5c44e9ee63774450a7cf7510842098ad0b6bf
Boateng Prince Agyenim <163312213+Mmabiaa@users.noreply.github.com>
2024-11-03 22:23:04 +0000

    Main.py

--- Chatbot/Main.py
+++ Chatbot/Main.py
@@ -2,6 +2,12 @@ from GUI import ChatApp
 import tkinter as tk
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    app = ChatApp(root)
-    root.mainloop()
+
+        root = tk.Tk()
+        app = ChatApp(root)
+        root.protocol("WM_DELETE_WINDOW", app.on_closing)  # Properly handle closing
+        # Configure tag styles for user and bot messages
+        app.chat_log.tag_configure("user", justify='right')
+        app.chat_log.tag_configure("bot", justify='left')
+        root.mainloop()
+    
